641b8082d5c86088e5cb62eae515c6417ff7714a
Jorge Resendiz Sanchez <egroj2_4@hotmail.com>
2018-05-06 17:58:42 -0500

    examen Jorge RS 6/05/2018

---
+++ js/examen.js
@@ -0,0 +1,48 @@
+function ocultarYa() {
+    var element = document.getElementById("esto");
+    element.classList.toggle("cambiar");
+}
+function ocultarYa1() {
+    var element = document.getElementById("esto1");
+    element.classList.toggle("cambiar");
+}
+function ocultarYa2() {
+    var element = document.getElementById("esto2");
+    element.classList.toggle("cambiar");
+}
+function ocultarYa3() {
+    var element = document.getElementById("esto3");
+    element.classList.toggle("cambiar");
+}
+function ocultarYa4() {
+    var element = document.getElementById("esto4");
+    element.classList.toggle("cambiar");
+}
+function ocultarYa5() {
+    var element = document.getElementById("esto5");
+    element.classList.toggle("cambiar");
+}
+function ocultarYa6() {
+    var element = document.getElementById("esto6");
+    element.classList.toggle("cambiar");
+}
+function ocultarYa7() {
+    var element = document.getElementById("esto7");
+    element.classList.toggle("cambiar");
+}
+function ocultarYa8() {
+    var element = document.getElementById("esto8");
+    element.classList.toggle("cambiar");
+}
+function ocultarYa9() {
+    var element = document.getElementById("esto9");
+    element.classList.toggle("cambiar");
+}
+function ocultarYa10() {
+    var element = document.getElementById("esto10");
+    element.classList.toggle("cambiar");
+}
+function ocultarYa11() {
+    var element = document.getElementById("esto11");
+    element.classList.toggle("cambiar");
+}
